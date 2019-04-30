f12b3b608d18d4d4c6b769b848960acea41784d9
Spawn <alkspawn@gmail.com>
2019-04-30 15:12:21 +0300

    Start

--- src/CommentDeleter.java
+++ src/CommentDeleter.java
@@ -1,2 +1,49 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class CommentDeleter {
+    //  D:\\Intelij project\\Lab10\\src\\TestJava.java Это я для теста использовал файл.
+
+    public File openFile() {
+        System.out.println("Enter the file directory: ");
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            try {
+                return new File(scanner.nextLine());
+            } catch (Exception e) {
+                System.out.println("Sorry. Try again. Error: " + e);
+            }
+        }
+        return null;
+    }
+
+    public String readFileAndDeletesComment(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        StringBuilder builder = new StringBuilder();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            line = line.replaceAll("//.+", "");
+            builder.append(line);
+            builder.append("///");
+        }
+        return builder.toString();
+    }
+
+    public void deletesMultiCommentAndWrite(String result, File file) throws IOException {
+        result = result.replaceAll("\\/\\*.*?\\*\\/", "");
+        FileWriter writer = new FileWriter(file);
+        String[] array = result.split("///");
+        for (String line : array) {
+            writer.write(line + "\n");
+        }
+        writer.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        CommentDeleter commentDeleter = new CommentDeleter();
+        File file = commentDeleter.openFile();
+        String result = commentDeleter.readFileAndDeletesComment(file);
+        commentDeleter.deletesMultiCommentAndWrite(result, file);
+    }
 }
